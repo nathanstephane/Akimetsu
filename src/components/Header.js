47d8711ea8c23480e382d9be5a91402d59ce19a4
nathanstephane <75221936+nathanstephane@users.noreply.github.com>
2022-06-23 22:34:04 +0200

    Noeuvale is an app to practice using the Library React. The goal is to make a call to a flight API and display the results.
    Initial commit.

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header">
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Favorites</li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
