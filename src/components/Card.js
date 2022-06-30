759e7e1a39286e8ad0e27d856b4454a0da78c671
nathanstephane <75221936+nathanstephane@users.noreply.github.com>
2022-06-30 22:31:58 +0200

    Card compoent-- tit;e, release date, Rating and Link to Youtube

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React from "react";
 
-const Card = ({anime}) => {
-    return (
-        <div className="card">
-            <img src={"https://media.kitsu.io/anime/poster_images/"+anime.id+"/medium.jpg"} alt=""/>
-            <h2>{anime.attributes.canonicalTitle}</h2>
-        </div>
-    );
+const Card = ({ anime }) => {
+  const formatDate = (date) => {
+    let [yy, mm, dd] = date.split("-"); //splits old date based off -
+    return [dd, mm, yy].join("/");
+  };
+
+  return (
+    <div className="card">
+      <img
+        src={
+          anime.attributes.posterImage.medium
+            ? anime.attributes.posterImage.medium
+            : "./img/noPoster.jpg"
+        }
+        alt="poster"
+      />
+      <h2>{anime.attributes.canonicalTitle}</h2>
+      {anime.attributes.startDate ? (
+        <h5>Released: {formatDate(anime.attributes.startDate)} </h5>
+      ) : (
+        ""
+      )}
+      {<h4>Rating: {anime.attributes.averageRating}%</h4>}
+      {<h5>Trailer: <a href={"https://www.youtube.com/watch?v="+anime.attributes.youtubeVideoId} target="_blank" rel="noopener noreferrer">Watch</a></h5>}
+
+     
+    </div>
+  );
 };
 
 export default Card;
